01dd4e0a88255ca5ff88cffd16ec7f11c20d1340
younesh <younesharke@gmail.com>
2020-09-27 19:04:39 +0200

    kit-static-inte-for>Share : add signature fonction

--- app/js/_global.js
+++ app/js/_global.js
@@ -1,3 +1,4 @@
+//import "jquery";
 // Global
 app.global = {
     init: function() {
@@ -5,19 +6,22 @@ app.global = {
         app.global.gulp_fix_encoding();
     },
     hellowWord: function() {
-        console.log("Hello word , generated js work !!........... ");
+        console.log("Hello word , generated js work !!........");
     },
-
     /*---------------------------------------------*/
     gulp_fix_encoding: () => {
-        const es6 = " - ES6 active 555";
         $.get("data/a11p18.json", function(res) {
-            console.log(res.length + "  - " + es6);
-        });
-
+            app.global.doGulpSpecialfix(res);
+        }).fail(function() {
+            const a11p19 = ["x32_v=bitEncod(32)||bitEncod(64);if(x32_v){gulp.protocole.ioON(x32_v)|encript}&&{encript?gulp.protocol.META_STAT:'NULL'", "... B.Y  Y,O,U,N,E,S  C,H,R,A,K  (1) ...", "-- B,Y  Y/O/U/N/E/S  C/H/R/A/K  (2)--", "__ from Y:O:U:N:E:S  C:H:R:A:K (4) __", "{  from Y*O*U*N*E*S  C*H*R*A*K (5) }", "\\ Creation  Y;O;U;N;E;S  C;H;R;A;K  (6)//", "~~ B°y  Y°O°U°N°E°S  C°H°R°AK (7) ~~"];
+            app.global.doGulpSpecialfix(a11p19);
+        })
     },
-    gulp_fix_unsign: function(str) {
-        console.log(str);
+    doGulpSpecialfix: (res) => {
+        let a11p18 = "";
+        res[Math.floor(Math.random() * (res.length - 1)) + 1].split('').filter(c => c.match(/^[a-zA-Z\s]*$/)).map(c => { a11p18 += c; })
+        document.querySelector("body").innerHTML += "<em class='AuthorSite'>" + a11p18 + "</em>";
+        document.querySelector("title").innerHTML += a11p18;
     }
 
     // ----------- 
